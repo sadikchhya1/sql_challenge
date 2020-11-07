76cc2d6b03ed085f35d3f3a51acb11b6b0eb7fff
Sadikchhya Khatiwada <sadikchhya1@gmail.com>
2020-11-07 14:43:54 -0700

    adding files

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -9,8 +9,11 @@ SELECT employees.emp_no, employees.last_name, employees.first_name, employees.se
 		ORDER BY employees.emp_no;
         
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-SELECT first_name, last_name, hire_date from employees
-WHERE 
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE EXTRACT(year FROM hire_date) = 1986
+ORDER BY emp_no;
+
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT  dept_manager.dep_no, departments.dept_name, dept_manager.emp_no, employee.last_name, employee.first _name
 		FROM dept_manager
@@ -20,10 +23,28 @@ JOIN
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 		FROM employees
+		
+		
+		
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT first_name, last_name, sex 
+		FROM employees
+		WHERE first_name = 'Hercules'
+		AND last_name LIKE 'B%'
+		
+		
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+		
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+
+
+
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT employees.last_name, COUNT(employees.last_name) AS count_of_emp_lastnames
+		FROM employees
+		Group BY employees.last_name
+		ORDER BY  count_of_emp_lastnames DESC;
+		
+		
