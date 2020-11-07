c4536d96176cf310327d5757ead4d7b56bc9c9e9
Sadikchhya Khatiwada <sadikchhya1@gmail.com>
2020-11-07 13:53:51 -0700

    Adding files

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,3 +1,5 @@
+
+-- DATA ANALYSIS
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
 		FROM employees
@@ -7,11 +9,17 @@ SELECT employees.emp_no, employees.last_name, employees.first_name, employees.se
 		ORDER BY employees.emp_no;
         
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-
+SELECT first_name, last_name, hire_date from employees
+WHERE 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT  dept_manager.dep_no, departments.dept_name, dept_manager.emp_no, employee.last_name, employee.first _name
+		FROM dept_manager
+JOIN
+
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+		FROM employees
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
