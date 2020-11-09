88e315ad3cab7de2182dc1954e2c952ffad35857
Sadikchhya Khatiwada <sadikchhya1@gmail.com>
2020-11-09 14:40:13 -0700

    Adding Files

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,5 +1,5 @@
-
 -- DATA ANALYSIS
+
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
 		FROM employees
